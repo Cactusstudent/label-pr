e1f4746cb8a5434cf647324f4f570fb0b9c9a537
Cactus_student <97865546+Cactusstudent@users.noreply.github.com>
2023-06-23 23:45:00 +0800

    Update labels.py

--- labels.py
+++ labels.py
@@ -1,4 +1,7 @@
 import subprocess
+import os
+from github import Github
+
 
 # 计算某个 commit 的变更行数
 def get_commit_lines(commit):
@@ -9,16 +12,36 @@ def get_commit_lines(commit):
     deleted = int(parts[3])
     return added - deleted
 
+
 # 计算所有 commit 的变更行数
 def get_total_lines(pr):
     commits = pr.get_commits()
     lines = sum(get_commit_lines(commit.sha) for commit in commits)
     return lines
 
-import json
 
-with open('config.json', 'r') as f:
-    config = json.load(f)
+if __name__ == '__main__':
+    # 获取 Pull Request 的对象
+    g = Github(os.getenv('GITHUB_TOKEN'))
+    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
+    pr_number = os.getenv('GITHUB_PULL_REQUEST_NUMBER')
+    pr = repo.get_pull(pr_number)
 
-labels = config['labels']
-thresholds = config['thresholds']
+    # 计算总行数并打标签
+    total_lines = get_total_lines(pr)
+    labels = []
+    if total_lines >= 1:
+        labels.append('1+')
+    if total_lines >= 40:
+        labels.append('40+')
+    if total_lines >= 100:
+        labels.append('100+')
+    if total_lines >= 500:
+        labels.append('500+')
+    if total_lines >= 1000:
+        labels.append('1000+')
+    if total_lines >= 2000:
+        labels.append('2000+')
+    if total_lines >= 5000:
+        labels.append('5000+')
+    pr.add_to_labels(*labels)
